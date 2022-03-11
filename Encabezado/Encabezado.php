66b9331d2f8958d80e2a23b2b615822bcb9844fc
Wishka <emanueljorgefernandez@gmail.com>
2022-03-11 16:14:25 -0300

    Update

---
+++ Encabezado/Encabezado.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="contenedor_titulo">
+        <h2 class="titulo">Escuela de Educación Secundaria Técnica N°1</h2>
+        <h2 class="subtitulo">Crucero Gral. Belgrano</h2>
+    </div>
+    <img src="./Imagenes/logo.png" alt="Logo" class="logo">
+</header>
